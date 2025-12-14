d922b586b11c748719d57e640695b7185d56526b
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-14 20:26:54 +0100

    chore: Remove explicit webpack configuration and `--webpack` flag from build scripts.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@koma/core", "@koma/database", "@koma/metadata"],
   reactCompiler: true,
-  webpack: (config) => {
-    config.resolve.extensionAlias = {
-      ".js": [".ts", ".tsx", ".js", ".jsx"],
-      ".mjs": [".mts", ".mjs"],
-      ".cjs": [".cts", ".cjs"],
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
