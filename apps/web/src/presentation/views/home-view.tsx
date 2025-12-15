64654c7b5d9d8f5f684330b66a2f9a2079202083
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-15 17:04:09 +0100

    feat: remove welcome back heading and description from home view

--- apps/web/src/presentation/views/home-view.tsx
+++ apps/web/src/presentation/views/home-view.tsx
@@ -8,15 +8,6 @@ export const HomeView = () => {
 
       <div className="relative">
         <main className="container mx-auto px-4 py-8 sm:px-6 lg:px-8 xl:px-12 2xl:max-w-480">
-          <div className="mx-auto max-w-2xl text-center">
-            <h1 className="font-mplus mb-2 text-4xl font-bold text-gray-900">
-              Welcome back
-            </h1>
-            <p className="mb-8 text-gray-500">
-              Find new comics to add to your collection.
-            </p>
-          </div>
-
           <div className="mb-12">
             <section className="w-full transition-all duration-500 ease-in-out">
               <SearchSection />
